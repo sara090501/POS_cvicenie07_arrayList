165747b62d64c7bd1a824fc6650854e5c33da285
geratova.sara <geratova.sara@gmail.com>
2022-11-06 14:50:30 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/POS_cvicenie07_arrayList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS_cvicenie07_arrayList.dir/main.c.o"
+  "POS_cvicenie07_arrayList"
+  "POS_cvicenie07_arrayList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POS_cvicenie07_arrayList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/POS_cvicenie07_arrayList.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for POS_cvicenie07_arrayList.
+# This may be replaced when dependencies are built.
